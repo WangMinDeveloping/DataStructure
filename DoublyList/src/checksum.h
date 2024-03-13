f5dd7e73367b66cd9012a49006197cbea9e8345e
wangmim1234 <94050933+wangmim1234@users.noreply.github.com>
2024-03-13 10:05:46 +0800

    Delete DoublyList/src directory

--- DoublyList/src/checksum.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-unsigned short checkSum(unsigned short * addr, int count);
--- DoublyList/src/invert.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-unsigned char* invertBytes(unsigned char *pSource,unsigned char *pDestination,int length);
-unsigned short invertShort(unsigned short wData);
-unsigned int invertInt(unsigned int uData);
