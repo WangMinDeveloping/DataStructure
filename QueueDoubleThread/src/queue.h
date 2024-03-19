59035e871a3934021b9b62aeaaeab167e5013ab4
wangmim1234 <2505287955@qq.com>
2024-03-19 17:14:13 +0800

    提交性能测试代码

--- QueueDoubleThread/src/queue.h
+++ QueueDoubleThread/src/queue.h
@@ -1,33 +1,61 @@
 #pragma once
+/*************************************************************************************
+ 功能简述：双线程安全队列，一入一出线程安全。
+ 注    意：队列初始化长度必须大于1
+ 作    者: WangMin
+ 时    间：2021-9
+ 版    本：1.0
+ *************************************************************************************/
 class queue
 {
 public:
     queue(/* args */);
     ~queue();
 
+  
     /*
-     队列初始化
-    输入:1、队列对象指针;2、队列长度
-    返回值:1、失败; 0、成功
-    */
+		@brief 				    队列初始化
+		@param 	iMaximum[in]：  队列初始化最大长度
+        @return	                true-成功  false-失败
+	*/
     bool initialize(int iMaximum);
 
+    /*
+		@brief 				    关闭函数
+		@param 
+        @return	                
+	*/
     void close();
+
+    
+    /*
+		@brief 				    获取队列最大长度
+		@param 
+        @return	                队列长度
+	*/
     int getQueueLength();
+
+    
+    /*
+		@brief 				    获取队列已有成员
+		@param 	
+        @return	                队列成员数量
+	*/
     int getQueueMemberCount();
 
     /*
-    入队函数:
-    输入：1、队列对象指针；2、入队变量指针 
-    返回值：0、成功；1、队满;
-    */
+		@brief 				    入队函数
+		@param 	pMember[in]：   队列指针
+        @return	                0-成功  1-队满
+	*/
     int enQueue(void* pMember);
 
+
     /*
-    出队函数
-    输入：1、队列对象指针；2、出队变量指针
-    返回值：0、成功;1、队空;
-    */
+		@brief 				    出队函数
+		@param 	pMember[out]：  出队指针的地址
+        @return	                0-成功  1-队空
+	*/
     int deQueue(void**pMember);
 
 private:
