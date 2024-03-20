8f923d77b5b8a9f466c029cf514e04f7d891dc1b
wangmim1234 <2505287955@qq.com>
2024-03-20 15:18:32 +0800

    提交性能测试代码，优化代码

--- RBTreeMultiValue/src/rbNode.h
+++ RBTreeMultiValue/src/rbNode.h
@@ -1,24 +1,25 @@
 #pragma once
+#include <stdlib.h>
 #define RED 0
 #define BLACK 1
 #include "CSinglyList.h"
-template<class KeyType,class ValueType>
+template <class KeyType, class ValueType>
 class CRedBlackTreeNode
 {
 public:
-        CRedBlackTreeNode<KeyType,ValueType> *pLeft;
-        CRedBlackTreeNode<KeyType,ValueType> *pRight;
-        CRedBlackTreeNode<KeyType,ValueType> *pFather;
+        CRedBlackTreeNode<KeyType, ValueType> *pLeft;
+        CRedBlackTreeNode<KeyType, ValueType> *pRight;
+        CRedBlackTreeNode<KeyType, ValueType> *pFather;
         KeyType key;
         bool bColor;
-        CSinglyList<ValueType>* pRecordList;//键重复时的链表
-        CRedBlackTreeNode(CRedBlackTreeNode<KeyType,ValueType> *pFatherNode=NULL)
+        CSinglyList<ValueType> *pRecordList; // 键重复时的链表
+        CRedBlackTreeNode(CRedBlackTreeNode<KeyType, ValueType> *pFatherNode = NULL)
         {
-                pLeft=NULL;
-                pRight=NULL;
-                pFather=pFatherNode;
-                bColor=BLACK;
-                pRecordList=NULL;//不支持键重复
+                pLeft = NULL;
+                pRight = NULL;
+                pFather = pFatherNode;
+                bColor = BLACK;
+                pRecordList = NULL; // 不支持键重复
         }
         ~CRedBlackTreeNode(){};
 };
---
+++ RBTreeMultiValue/test/testPerformance.cpp
@@ -0,0 +1,51 @@
+#include "../src/rbTree.h"
+#include <stdio.h>
+#include <sys/time.h>
+#include <string.h>
+#include <time.h>
+#include <map>
+#define loopTimes (1024)
+long long GetTime()
+{
+    long long tmp;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    tmp = tv.tv_sec;
+    tmp = tmp * 1000000;
+    tmp = tmp + tv.tv_usec;
+    return tmp;
+}
+
+int TestStlMap()
+{
+    std::multimap<int, int> stlMap;
+    long long  beginTime = GetTime();
+    for (int i = 0; i < loopTimes; i++)
+    {
+        stlMap.insert(std::make_pair(i,i));
+    }
+    long long  endTime = GetTime();
+    printf("multi map cross time  %lld.\n", endTime - beginTime);
+    return 0;
+}
+
+int TestMymap()
+{
+    CRedBlackTree<int, int> myTree;
+    long long  beginTime = GetTime();
+    for (int  i = 0; i < loopTimes; i++)
+    {
+        myTree.insert(i, i);
+    }
+    long long  endTime = GetTime();
+    printf(" my RBTree cross time  %lld.\n", endTime - beginTime);
+    return 0;
+}
+
+int main()
+{
+    TestStlMap();
+    TestMymap();
+    return 0;
+}
+ 
