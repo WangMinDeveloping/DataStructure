6e475b5a7d14f8448b6b401ae46701d81df633ab
wangmim1234 <2505287955@qq.com>
2024-03-21 16:48:18 +0800

    优化性能

--- DoublyList/include/doublyList.h
+++ DoublyList/include/doublyList.h
@@ -1,100 +1,169 @@
 #pragma once
-#include"doublyListNode.h"
-template<class TNodeType>
+#include "doublyListNode.h"
+#include "doublyListIterator.h"
+
+#define InitNodeNum (512)
+#define PieceCount (1024*1024)
+
+/*************************************************************************************
+ 功能简述：模板双向链表，带有与stl一致的迭代器
+ 使用场景：需要多线程高性能开发的场景，经测试性能高于stl库
+ 作    者: WangMin
+ 时    间：2021-8
+ 版    本：1.0
+ *************************************************************************************/
+template <class TNodeType>
 class CDoublyList
 {
 public:
-		typedef CDoublyListNode<TNodeType> iterator;
-		CDoublyList();	
-		~CDoublyList();
+	typedef DoublyListIterator<TNodeType> iterator;
+	CDoublyList();
+	~CDoublyList();
 
-		/*
-		@brief 					双向链表头插法
-		@param 	value[in]： 	插入的值
-		@return CDoublyListNode<TNodeType>*		
-				大于0 该数据存入的链表结点地址；0 new分配空间失败
-		*/
-		CDoublyListNode<TNodeType>* insert(TNodeType value);
+	/*
+	@brief 					双向链表尾插法
+	@param 	value[in]： 	插入的值
+	@return CDoublyListNode<TNodeType>*
+			大于0 该数据存入的链表结点地址；0 new分配空间失败
+	*/
+	CDoublyListNode<TNodeType> *insertTail(TNodeType value);
 
-		/*
-		@brief 				删除节点函数
-		@param 	pNode[in]： 被删除点的子结点
-		@return int：		0 成功  1 失败（参数无效）
-		*/
-		int deleteNode(CDoublyListNode<TNodeType>* pNode);
+	/*
+	@brief 				删除节点函数
+	@param 	pNode[in]： 被删除点的子结点
+	@return int：		0 成功  1 失败（参数无效）
+	*/
+	int deleteNode(CDoublyListNode<TNodeType> *pNode);
 
-		/*
-		@brief 				返回双向链表第一个节点
-		@param
-		@return CDoublyListNode<TNodeType>&: 第一个链表节点引用
-		*/
-		CDoublyListNode<TNodeType>& begin();
+	/*
+	@brief 				返回双向链表第一个节点
+	@param
+	@return CDoublyListNode<TNodeType>&: 第一个链表节点引用
+	*/
+	DoublyListIterator<TNodeType> begin();
 
-		/*
-		@brief 				返回尾节点
-		@param
-		@return CDoublyListNode<TNodeType>& 尾节点
-		*/
-		CDoublyListNode<TNodeType>& end();
+	/*
+	@brief 				返回尾节点
+	@param
+	@return CDoublyListNode<TNodeType>& 尾节点
+	*/
+	DoublyListIterator<TNodeType>& end();
 
 private:
-		CDoublyListNode<TNodeType> *pHeadNode;
-		CDoublyListNode<TNodeType> *pTailNode;
-
+	CDoublyListNode<TNodeType> *pHeadNode;
+	CDoublyListNode<TNodeType> *pTailNode;
+	CDoublyListNode<TNodeType> *pCurrentNode; //当前的空节点 ，用户插入节点
+	CDoublyListNode<TNodeType>** pieceArray;
+	unsigned long long pieceNum;
+	iterator tailIterator;
 };
 
 template <class TNodeType>
 CDoublyList<TNodeType>::CDoublyList()
 {
-	//生成一个头节点和一个尾节点
-	pHeadNode = new CDoublyListNode<TNodeType>();//插入函数统一代码，不然每次插入都需要判断pheadNode是否存在，降低效率，只有双向链表需要
-	pTailNode = new CDoublyListNode<TNodeType>();//目的是增加一个结束标注，方便迭代器的使用end()
-	pHeadNode->pNext = pTailNode;
-	pTailNode->pPrev = pHeadNode;
+	CDoublyListNode<TNodeType> *p  = new CDoublyListNode<TNodeType>[InitNodeNum]();
+	pieceArray  = new CDoublyListNode<TNodeType>*[PieceCount]();
+	for (int i = 0; i < PieceCount; i++)
+	{
+		pieceArray[i] = nullptr;
+	}
+
+	pHeadNode = &p[0];
+	pHeadNode->pNext = &p[1];
+	pHeadNode->pPrev = nullptr;
+
+	for (int i = 1; i < InitNodeNum - 1; i++)
+	{
+		p[i].pNext = &p[i+1];
+		p[i].pPrev = &p[i-1];
+	}
+	
+	pTailNode = &p[InitNodeNum - 1];
+	pTailNode->pPrev = &p[InitNodeNum - 2];
+	pTailNode->pNext = nullptr;
+
+	pCurrentNode = &p[0];
+
+	pieceNum = 0;
+	pieceArray[pieceNum++] = p;
 }
 
 template <class TNodeType>
 CDoublyList<TNodeType>::~CDoublyList()
 {
-	//删除所有结点
+	// 删除所有结点
 	CDoublyListNode<TNodeType> *pSeekNode = pHeadNode;
 	CDoublyListNode<TNodeType> *pNode;
 
-	while (pSeekNode != 0)
+	int i = 0;
+	while (i < PieceCount && pieceArray[i] != 0)
 	{
-		pNode = pSeekNode->pNext; // 保存删除的下一个结点
-		delete pSeekNode;
-		pSeekNode = pNode;
+		delete[] pieceArray[i];
+		pieceArray[i] = nullptr;
+		i++;
 	}
-	pHeadNode = 0;
 }
 
 template <class TNodeType>
-CDoublyListNode<TNodeType>& CDoublyList<TNodeType>::begin()
+DoublyListIterator<TNodeType> CDoublyList<TNodeType>::begin()
 {
-	return *(pHeadNode->pNext);
+	iterator headIterator;
+	headIterator.setValue(pHeadNode->pNext);
+	return headIterator;
 }
 
 template <class TNodeType>
-CDoublyListNode<TNodeType> &CDoublyList<TNodeType>::end()
+DoublyListIterator<TNodeType>& CDoublyList<TNodeType>::end()
 {
-	return *pTailNode;
+	tailIterator.setValue(pCurrentNode);
+	return tailIterator;
 }
 
+//尾插法
 template <class TNodeType>
-CDoublyListNode<TNodeType> *CDoublyList<TNodeType>::insert(TNodeType value)
+CDoublyListNode<TNodeType> *CDoublyList<TNodeType>::insertTail(TNodeType value)
 {
-	CDoublyListNode<TNodeType> *pNewNode = new CDoublyListNode<TNodeType>();
-	if (!pNewNode)
-		return 0;
+	CDoublyListNode<TNodeType> *pRetNode = nullptr;
+	if (pCurrentNode != pTailNode)
+	{
+		pCurrentNode->value = value;
+		pRetNode = pCurrentNode;
+		pCurrentNode = pCurrentNode->pNext;
+	}
+	else
+	{
+		CDoublyListNode<TNodeType> *p  = new CDoublyListNode<TNodeType>[InitNodeNum]();
+		if (pieceNum < PieceCount)
+		{
+			pieceArray[pieceNum++] = p;
+		}
+		else
+		{
+			return 0;
+		}
 
-	pNewNode->pNext = pHeadNode->pNext;
-	pHeadNode->pNext->pPrev = pNewNode;
-	
-	pNewNode->value = value;
-	pNewNode->pPrev = pHeadNode;
-	pHeadNode->pNext = pNewNode;
-	return pNewNode;
+		//处理中间部分，防止数组越界
+		for (int i = 1; i < InitNodeNum - 1; i++)
+		{
+			p[i].pNext = &p[i+1];
+			p[i].pPrev = &p[i-1];
+		}
+		
+		pCurrentNode = &p[0];
+		pCurrentNode->pNext = &p[1];
+		pCurrentNode->pPrev = pTailNode->pPrev;
+		pTailNode->pPrev->pNext = pCurrentNode;
+
+		pTailNode->pPrev = &p[InitNodeNum - 1];
+		p[InitNodeNum - 1].pNext = pTailNode;
+		p[InitNodeNum - 1].pPrev = &p[InitNodeNum - 2];
+
+		pCurrentNode->value = value;
+		pRetNode = pCurrentNode;
+		pCurrentNode = pCurrentNode->pNext;
+	}
+
+	return pRetNode;
 }
 
 template <class TNodeType>
--- DoublyList/src/doublyListNode.h
+++ DoublyList/src/doublyListNode.h
@@ -6,37 +6,4 @@ public:
 	CDoublyListNode<TDataType> *pNext;
 	CDoublyListNode<TDataType> *pPrev;
 	TDataType value;
-	CDoublyListNode<TDataType>& operator++();
-	CDoublyListNode<TDataType>& operator +(int num);
-	bool operator !=(CDoublyListNode<TDataType> &p);
-	TDataType& operator*();
 };
-
-template<class TDataType>
-CDoublyListNode<TDataType>& CDoublyListNode<TDataType>::operator ++()
-{
-	return *(this->pNext);
-}
-
-template<class TDataType>
-CDoublyListNode<TDataType>& CDoublyListNode<TDataType>::operator +(int num)
-{
-	CDoublyListNode<TDataType> *pNode=this;
-	while(num--)
-	{
-		pNode=pNode->pNext;
-	}
-	return *pNode;
-}
-
-template<class TDataType>
-TDataType& CDoublyListNode<TDataType>::operator*()
-{
-	return this->value;
-}
-
-template<class TDataType>
-bool CDoublyListNode<TDataType>::operator !=(CDoublyListNode<TDataType> &p)
-{
-	return this->pNext != p.pNext;
-}
